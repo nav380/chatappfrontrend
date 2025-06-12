ce462bebd5d1530f38a865ac409da8c6994fb47d
Naveen chauhan <143591216+nav380@users.noreply.github.com>
2025-06-12 20:37:03 +0530

    Update config.jsx

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,7 +1,7 @@
 const DjangoConfig = {
-    apiUrl:`http://192.168.1.8:9877/`,  
-    wsUrl:`ws://192.168.1.8:9877/`, 
-    profile_picture_url:`http://192.168.1.8:9877`
+    apiUrl:`http://  /`,  
+    wsUrl:`ws:// /`, 
+    profile_picture_url:`http:// `
 };
 
 export default DjangoConfig;
